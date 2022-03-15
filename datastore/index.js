6e73a004560e2606c5c03373f20363a372c80656
Elliot Landon <elliotlandon5@gmail.com>
2022-03-15 11:55:23 -0700

    Complete updating a todoComplete updating a todo

--- datastore/index.js
+++ datastore/index.js
@@ -35,6 +35,14 @@ exports.readAll = (callback) => {
         result.id = id[0];
         result.text = id[0];
         data.push(result);
+        // var result = {};
+        // var fullPath = path.join(exports.dataDir, title);
+        // var id = title.split('.');
+        // fs.readFile(fullPath, (err, data) => {
+        //   result.id = id[0];
+        //   result.text = data;
+        //   data.push(result);
+        // });
       });
       callback(null, data);
     }
@@ -42,33 +50,67 @@ exports.readAll = (callback) => {
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+
+  var result = {};
+  var name = id + '.txt';
+  var fullPath = path.join(exports.dataDir, name);
+
+  fs.readFile(fullPath, 'utf8', (err, data) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      result.id = id;
+      result.text = data;
+      callback(null, result);
+    }
+  });
 };
 
+
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+
+  var name = id + '.txt';
+  var fullPath = path.join(exports.dataDir, name);
+
+  fs.readFile(fullPath, 'utf8', (err, data) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.writeFile(fullPath, text, 'utf8', (err) => {
+        if (err) {
+          throw ('cannot update file');
+        } else {
+          callback(null, { id, text });
+        }
+      });
+    }
+  });
+
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+
+  var result = {};
+  var name = id + '.txt';
+  var fullPath = path.join(exports.dataDir, name);
+
+  fs.readFile(fullPath, (err, data) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      delete result.id;
+      callback();
+    }
+  });
+
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
